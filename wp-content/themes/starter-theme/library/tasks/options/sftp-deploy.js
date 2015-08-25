64af2d25c7be4699db111be2280ef5007bc1195e
Brian Maier Jr <brimaidesigns@gmail.com>
2015-08-25 11:36:41 -0400

    + deploy-production

--- wp-content/themes/starter-theme/library/tasks/options/sftp-deploy.js
+++ wp-content/themes/starter-theme/library/tasks/options/sftp-deploy.js
@@ -1,10 +1,32 @@
 module.exports = {
   'sftp-deploy': {
-    build: {
+    staging: {
       auth: {
         host: '11.11.11.11',
         port: 22,
-        authKey: 'staging' 
+        authKey: 'staging'
+      },
+      cache: 'sftpCache.json',
+      src: '/Users/user/Sites/starter-theme/wp-content/themes/starter-theme',
+      dest: 'wp-content/themes/starter-theme',
+      exclusions: [
+        '.ftppass',
+        '.git',
+        '.gitignore',
+        'node_modules',
+        '.sass-cache',
+        'npm-debug.log',
+        '.DS_Store',
+        '.sftpcache.json'
+      ],
+      concurrency: 4,
+      progress: true
+    }
+    production: {
+      auth: {
+        host: '11.11.11.11',
+        port: 22,
+        authKey: 'production' 
       },
       cache: 'sftpCache.json',
       src: '/Users/user/Sites/starter-theme/wp-content/themes/starter-theme',
