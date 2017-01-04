fb5f934dd788cf139596fc8e3825b14cb6d309ae
Brian Maier Jr <brimaidesigns@gmail.com>
2017-01-04 16:06:38 -0500

    update critical

--- wp-content/themes/starter-theme/library/tasks/deploy.js
+++ wp-content/themes/starter-theme/library/tasks/deploy.js
@@ -1,3 +1,3 @@
 module.exports = function(grunt) {
-  grunt.registerTask('deploy', ['sass', 'criticalcss', 'autoprefixer', 'cssmin', 'concat', 'uglify', 'imagemin', 'sassdoc']);
+  grunt.registerTask('deploy', ['sass', 'criticalcss', 'autoprefixer', 'cssmin', 'concat', 'uglify', 'sassdoc']);
 };
