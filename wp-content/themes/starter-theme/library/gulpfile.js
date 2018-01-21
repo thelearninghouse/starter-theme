d9d51d3377904e5e520789e061083486f9aa0824
David Royer <droyer01@gmail.com>
2018-01-21 10:57:24 -0500

    update

--- wp-content/themes/starter-theme/library/gulpfile.js
+++ wp-content/themes/starter-theme/library/gulpfile.js
@@ -1,4 +1,5 @@
 // Require Modules
+const pkg = require('./package.json');
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
@@ -14,119 +15,98 @@ var rename = require('gulp-rename');
 var gulp = require('gulp');
 var fancyLog = require('fancy-log')
 var pa11y = require('pa11y')
+var fs = require('fs')
+var htmlReporter = require('pa11y/reporter/html');
+var a11yResultsDirectory = 'accessibility'
+var a11yTest = pa11y({
+	// Log what's happening to the console
+	log: {
+		debug: console.log.bind(console),
+		error: console.error.bind(console),
+		info: console.log.bind(console)
+	}
+});
 
 var paths = {
-  sass: {
-    src: ['scss/*.scss', 'scss/**/*.scss'],
-    dest: 'css/',
-  },
-  scripts: {
-    src: ['js/libs/*.js', 'js/scripts.js'],
-    dest: 'js/build/',
-  },
-  imagemin: {
-    src: 'images/*',
-    dest: 'images/',
-  }
+	sass: {
+		src: ['scss/*.scss', 'scss/**/*.scss'],
+		dest: 'css/',
+	},
+	scripts: {
+		src: ['js/libs/*.js', 'js/scripts.js'],
+		dest: 'js/build/',
+	},
+	imagemin: {
+		src: 'images/*',
+		dest: 'images/',
+	}
 };
 
-// Process data in an array synchronously, moving onto the n+1 item only after the nth item callback
-function doSynchronousLoop(data, processData, done) {
-    if (data.length > 0) {
-        const loop = (data, i, processData, done) => {
-            processData(data[i], i, () => {
-                if (++i < data.length) {
-                    loop(data, i, processData, done);
-                } else {
-                    done();
-                }
-            });
-        };
-        loop(data, 0, processData, done);
-    } else {
-        done();
-    }
-}
-
-// Run pa11y accessibility tests on each template
-function processAccessibility(element, i, callback) {
-    const accessibilitySrc = pkg.urls;
-    const cliReporter = require('./node_modules/pa11y/reporter/cli.js');
-    const options = {
-        log: cliReporter,
-        ignore:
-                [
-                    'notice',
-                    'warning'
-                ],
-        };
-    const test = pa11y(options);
-
-    fancyLog("-> Checking Accessibility for URL: " + chalk.cyan(accessibilitySrc));
-    test.run(accessibilitySrc, (error, results) => {
-        cliReporter.results(results, accessibilitySrc);
-        callback();
-    });
-}
-
-// accessibility task
+// ACCESSIBILITY TEST TASK
 gulp.task("a11y", (callback) => {
-    doSynchronousLoop(pkg.urls, processAccessibility, () => {
-        // all done
-        callback();
-    });
+	handleResultsDirectoryPath();
+	handleUrlsArray(pkg.urls);
+	callback();
 });
 
 // BROWSERSYNC
 gulp.task('browser-sync', function() {
-    browserSync.init({
-        proxy: "starter-git.dev"
-    });
+	browserSync.init({
+		proxy: "starter-git.dev"
+	});
 });
 
 // SASS
 gulp.task('sass', function() {
-  return gulp.src(paths.sass.src)
-    .pipe(sass()) // Using gulp-sass
-    .on('error', onError)
-    .pipe(autoprefixer())
-    .pipe(gulp.dest(paths.sass.dest))
-    .pipe(minifyCss({compatibility: 'ie9'}))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest(paths.sass.dest))
-    .pipe(browserSync.stream());
+	return gulp.src(paths.sass.src)
+		.pipe(sass()) // Using gulp-sass
+		.on('error', onError)
+		.pipe(autoprefixer())
+		.pipe(gulp.dest(paths.sass.dest))
+		.pipe(minifyCss({
+			compatibility: 'ie9'
+		}))
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(gulp.dest(paths.sass.dest))
+		.pipe(browserSync.stream());
 });
 
 // CONCAT JS FILES
 gulp.task('scripts', function() {
-  return gulp.src(paths.scripts.src)
-    .pipe(concat('production.js'))
-    .pipe(gulp.dest(paths.scripts.dest))
-    .pipe(uglify())
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest(paths.scripts.dest))
-    .pipe(browserSync.stream());
+	return gulp.src(paths.scripts.src)
+		.pipe(concat('production.js'))
+		.pipe(gulp.dest(paths.scripts.dest))
+		.pipe(uglify())
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(gulp.dest(paths.scripts.dest))
+		.pipe(browserSync.stream());
 });
 
 // IMAGEMIN
-gulp.task('imagemin', function () {
-    return gulp.src(paths.imagemin.src)
-        .pipe(imagemin({
-            progressive: true,
-            svgoPlugins: [{removeViewBox: false}],
-            use: [pngquant()]
-        }))
-        .pipe(gulp.dest(paths.imagemin.dest));
+gulp.task('imagemin', function() {
+	return gulp.src(paths.imagemin.src)
+		.pipe(imagemin({
+			progressive: true,
+			svgoPlugins: [{
+				removeViewBox: false
+			}],
+			use: [pngquant()]
+		}))
+		.pipe(gulp.dest(paths.imagemin.dest));
 });
 
 // WATCH STYLES
- gulp.task('watch:styles', function () {
-   gulp.watch(paths.sass.src, gulp.series('sass'));
+gulp.task('watch:styles', function() {
+	gulp.watch(paths.sass.src, gulp.series('sass'));
 });
 
 // WATCH SCRIPTS
- gulp.task('watch:scripts', function () {
-   gulp.watch(paths.scripts.src, gulp.series('scripts'));
+gulp.task('watch:scripts', function() {
+	gulp.watch(paths.scripts.src, gulp.series('scripts'));
 });
 
 // WATCH
@@ -136,6 +116,35 @@ gulp.task('default', gulp.parallel('browser-sync', 'watch:styles', 'watch:script
 gulp.task('deploy', gulp.parallel('sass', 'scripts', 'imagemin'));
 
 function onError(err) {
-  console.log(err);
-  this.emit('end');
+	console.log(err);
+	this.emit('end');
+}
+
+function handleResultsDirectoryPath() {
+	if (!fs.existsSync(`${pkg.resultsDirectory}`)) {
+		fs.mkdirSync(`${pkg.resultsDirectory}`);
+	}
+}
+
+function handleTestResults(resultFilePath, htmlOutput) {
+	fs.writeFile(resultFilePath, htmlOutput, function(err) {
+		if (err) throw err;
+	});
+}
+
+function handleUrlsArray(urls) {
+	for (let currentIndex = 0; currentIndex < urls.length; currentIndex++) {
+		let url = urls[currentIndex];
+		let resultFilePath = `${pkg.resultsDirectory}/a11yTest-${currentIndex}.html`
+
+		a11yTest.run(url, function(error, results) {
+			if (error) {
+				return console.error(error.message)
+			}
+
+			let html = htmlReporter.process(results, url);
+			handleTestResults(resultFilePath, html)
+
+		});
+	}
 }
