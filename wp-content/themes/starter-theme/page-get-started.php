83ff2c9cc47917ba37a3d10423065f0e7d8be62e
Brian Maier Jr <brimaidesigns@gmail.com>
2018-03-08 16:51:52 -0500

    + thank you page template and styling + get started page and styling

---
+++ wp-content/themes/starter-theme/page-get-started.php
@@ -0,0 +1,35 @@
+<?php
+/*
+Template Name: Get started
+*/
+get_header(); ?>
+
+	<?php get_template_part( 'template-parts/hero_title' ); ?>
+
+	<div class="content">
+
+		<div class="wrap">
+
+				<main id="content" class="main-content cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+
+					<div class="sidebar-well">
+						<h2 class="h3">Get Started</h2>
+						<script src="https://requestforms.learninghouse.com/form/show/rivier-university/ppc-form-multi/600/3342/online.rivier.edu:thank-you:request_id" type="text/javascript"></script>
+					</div>
+
+					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+					<section class="page-content cf" itemprop="articleBody">
+						<?php the_content(); ?>
+					</section> <?php // end article section ?>
+
+					<?php endwhile; endif; ?>
+
+				</main>
+
+
+		</div>
+
+	</div>
+
+<?php get_footer(); ?>
