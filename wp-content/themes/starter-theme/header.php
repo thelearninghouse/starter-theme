577efa230cd95e605787b5c75acfdca94a2b89c4
Brian Maier Jr <brimaidesigns@gmail.com>
2018-03-19 14:04:58 -0400

    edit favicon

--- wp-content/themes/starter-theme/header.php
+++ wp-content/themes/starter-theme/header.php
@@ -15,7 +15,13 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 
 		<?php // icons & favicons (generate here: https://realfavicongenerator.net/) (for more: http://www.jonathantneal.com/blog/understand-the-favicon/) ?>
-		<link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
+		<link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png">
+		<link rel="manifest" href="/favicons/site.webmanifest">
+		<link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#b12028">
+		<meta name="msapplication-TileColor" content="#b12028">
+		<meta name="theme-color" content="#ffffff">
 
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
