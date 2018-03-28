c8b211ce59570a186c96d310f55b0ab62feb9916
Kurt Rank <krank@learninghouse.com>
2018-03-28 12:12:31 -0400

    Fix grid layout on pages with sidebar, fix accordion styling

--- wp-content/themes/starter-theme/page-get-started.php
+++ wp-content/themes/starter-theme/page-get-started.php
@@ -7,18 +7,21 @@ get_header(); ?>
 <main id="content" class="main-content" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
 		<?php get_template_part( 'template-parts/hero_title' ); ?>
 
-	<div class="sidebar-well">
-		<h2 class="h3">Get Started</h2>
-		<script src="https://requestforms.learninghouse.com/form/show/rivier-university/ppc-form-multi/600/3342/online.rivier.edu:thank-you:request_id" type="text/javascript"></script>
-	</div>
+		<div class="wrap page-wrapper">
 
-	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+			<div class="sidebar-well">
+				<h2 class="h3">Get Started</h2>
+				<script src="https://requestforms.learninghouse.com/form/show/rivier-university/ppc-form-multi/600/3342/online.rivier.edu:thank-you:request_id" type="text/javascript"></script>
+			</div>
 
-	<section class="page-content cf" itemprop="articleBody">
-		<?php the_content(); ?>
-	</section> <?php // end article section ?>
+			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-	<?php endwhile; endif; ?>
+			<section class="page-content cf" itemprop="articleBody">
+				<?php the_content(); ?>
+			</section> <?php // end article section ?>
+
+			<?php endwhile; endif; ?>
+		</div>
 
 </main>
 
