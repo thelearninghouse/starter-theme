de37e657d3dd1ca7dbd3a27614fa54ba8b015a0a
David Royer <droyer01@gmail.com>
2018-05-24 12:29:00 -0400

    update to php

--- wp-content/themes/starter-theme/inc/degree-filter-helpers.php
+++ wp-content/themes/starter-theme/inc/degree-filter-helpers.php
@@ -1,8 +1,8 @@
 <?php
   function setFilterOrder($term1, $term2) {
-      if ($term1->display_order == $term2->display_order) {
+      if ($term1->display_position == $term2->display_position) {
           return 0;
-      } elseif ($term1->display_order < $term2->display_order) {
+      } elseif ($term1->display_position < $term2->display_position) {
           return -1;
       } else {
           return 1;
@@ -29,10 +29,8 @@
       ]);
       $sub_areas = $term_children ? $term_children : array();
       $acf_term_id = 'term_' . $term_id;
-      $displayOrder = get_field('display_order', $acf_term_id) ?  get_field('display_order', $acf_term_id) : '0';
-
       $term->sub_areas = $sub_areas;
-      $term->display_order = $displayOrder;
+
     }
     return $degreeFilterArray;
   }
@@ -54,14 +52,18 @@
     return $degrees;
   }
 
+
   function buildDegreeLevels($degreeLevels) {
+
     foreach ( $degreeLevels as $term ) {
       $array = array();
       $term_id = $term->term_id;
       $acf_term_id = 'term_' . $term_id;
-      $displayOrder = get_field('display_order', $acf_term_id) ?  get_field('display_order', $acf_term_id) : '0';
-      $term->display_order = $displayOrder;
+      $displayPosition = get_term_meta( $term_id, 'tax_position', true ) ? get_term_meta( $term_id, 'tax_position', true ) : '0';
+      $term->display_position = $displayPosition;
     }
+
+    usort($degreeLevels, 'setFilterOrder');
     return $degreeLevels;
   }
 
@@ -74,6 +76,7 @@
        'post_type' => 'degrees',
        'post_status' => 'publish',
     );
+
 
     $allDegrees = get_posts( $allDegreesArgs );
     $degree_areas = get_field('degree_area_filters', 'option') ? get_field('degree_area_filters', 'option') : array();
@@ -89,76 +92,85 @@
   }
 
   function buildDegreeFilteringSettingsPage() {
-    acf_add_local_field_group(array(
-    	'key' => 'group_5ab4073782723',
-    	'title' => 'Degree Filtering Plugin Settings',
-    	'fields' => array(
-    		array(
-    			'key' => 'field_5ab407560ce91',
-    			'label' => 'Degree Area Filters',
-    			'name' => 'degree_area_filters',
-    			'type' => 'taxonomy',
-    			'instructions' => '',
-    			'required' => 0,
-    			'conditional_logic' => 0,
-    			'wrapper' => array(
-    				'width' => '',
-    				'class' => '',
-    				'id' => '',
-    			),
-    			'taxonomy' => 'degree_area',
-    			'field_type' => 'checkbox',
-    			'allow_null' => 0,
-    			'add_term' => 1,
-    			'save_terms' => 0,
-    			'load_terms' => 0,
-    			'return_format' => 'object',
-    			'multiple' => 0,
-    		),
-    		array(
-    			'key' => 'field_5ab407adcc248',
-    			'label' => 'Degree Level Filters',
-    			'name' => 'degree_level_filters',
-    			'type' => 'taxonomy',
-    			'instructions' => '',
-    			'required' => 0,
-    			'conditional_logic' => 0,
-    			'wrapper' => array(
-    				'width' => '',
-    				'class' => '',
-    				'id' => '',
-    			),
-    			'taxonomy' => 'degree_level',
-    			'field_type' => 'checkbox',
-    			'allow_null' => 0,
-    			'add_term' => 0,
-    			'save_terms' => 0,
-    			'load_terms' => 0,
-    			'return_format' => 'object',
-    			'multiple' => 0,
-    		),
-    	),
-    	'location' => array(
-    		array(
-    			array(
-    				'param' => 'options_page',
-    				'operator' => '==',
-    				'value' => 'theme-general-settings',
-    			),
-    		),
-    	),
-    	'menu_order' => 0,
-    	'position' => 'normal',
-    	'style' => 'default',
-    	'label_placement' => 'top',
-    	'instruction_placement' => 'label',
-    	'hide_on_screen' => '',
-    	'active' => 1,
-    	'description' => '',
-    ));
+
+    if( function_exists('acf_add_options_page') ) {
+      acf_add_options_sub_page(array(
+        'page_title' 	=> 'Degree Filtering Plugin Settings',
+        'menu_title'	=> 'Degree Filtering Plugin',
+        'parent_slug'	=> 'theme-settings',
+      ));
+    }
+
+    if( function_exists('acf_add_local_field_group') ) {
+      acf_add_local_field_group(array(
+        'key' => 'group_5ab4073782723',
+        'title' => 'Degree Filtering Plugin Settings',
+        'fields' => array(
+          array(
+            'key' => 'field_5ab407560ce91',
+            'label' => 'Degree Area Filters',
+            'name' => 'degree_area_filters',
+            'type' => 'taxonomy',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+              'width' => '',
+              'class' => '',
+              'id' => '',
+            ),
+            'taxonomy' => 'degree_area',
+            'field_type' => 'checkbox',
+            'allow_null' => 0,
+            'add_term' => 1,
+            'save_terms' => 0,
+            'load_terms' => 0,
+            'return_format' => 'object',
+            'multiple' => 0,
+          ),
+          array(
+            'key' => 'field_5ab407adcc248',
+            'label' => 'Degree Level Filters',
+            'name' => 'degree_level_filters',
+            'type' => 'taxonomy',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+              'width' => '',
+              'class' => '',
+              'id' => '',
+            ),
+            'taxonomy' => 'degree_level',
+            'field_type' => 'checkbox',
+            'allow_null' => 0,
+            'add_term' => 0,
+            'save_terms' => 0,
+            'load_terms' => 0,
+            'return_format' => 'object',
+            'multiple' => 0,
+          ),
+        ),
+        'location' => array(
+          array(
+            array(
+              'param' => 'options_page',
+              'operator' => '==',
+              'value' => 'acf-options-degree-filtering-plugin',
+            ),
+          ),
+        ),
+        'menu_order' => 0,
+        'position' => 'normal',
+        'style' => 'default',
+        'label_placement' => 'top',
+        'instruction_placement' => 'label',
+        'hide_on_screen' => '',
+        'active' => 1,
+        'description' => '',
+      ));
+    }
   }
 
-  if( function_exists('acf_add_local_field_group') ):
-    buildDegreeFilteringSettingsPage();
-  endif;
+  buildDegreeFilteringSettingsPage();
 ?>
