a6b6a431993ce10696fd029261a23e01a41c1f13
David Royer <droyer01@gmail.com>
2018-06-03 07:18:20 -0400

    initial commit

---
+++ wp-content/themes/starter-theme/.DOCUMENTATION/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  title: "Starter Theme",
+  description: "Wordpress Theme for TLH Marketing Sites",
+  themeConfig: {
+    editLinks: true,
+    docsDir: ".DOCUMENTATION",
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Getting Started", link: "/getting-started/" },
+      { text: "FAQ", link: "/faq/" }
+    ]
+  },
+  dest: 'docs'
+};
