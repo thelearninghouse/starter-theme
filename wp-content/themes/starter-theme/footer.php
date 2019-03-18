569058121864ca8ea7265298e877a2fed5fa17cd
Brian Maier Jr <brimaidesigns@gmail.com>
2019-03-18 09:14:44 -0400

    fix footer conditionals

--- wp-content/themes/starter-theme/footer.php
+++ wp-content/themes/starter-theme/footer.php
@@ -18,10 +18,10 @@
 					</nav>
 
 					<p class="footer__copyright">&copy; <?php echo date('Y'); ?><?php bloginfo( 'name' ); ?>
-						<?php $address1 = the_field('school_address_1', 'option') ? ' | ' . $address1 : ''; ?>
-						<?php $address2 = the_field('school_address_2', 'option') ? ' | ' . $address2 : ''; ?>
-						<?php $phone = the_field('school_phone', 'option') ? ' | ' . $phone : '' ?>
-						<?php $email = the_field('school_email_address', 'option') ? ' | <a href="mailto:' . $email . '">' . $email . '</a>' : ''; ?>
+						<?php if (get_field('school_address_1', 'option') ) { echo ' | '; the_field('school_address_1', 'option'); } ?>
+						<?php if (get_field('school_address_2', 'option') ) { the_field('school_address_2', 'option'); } ?>
+						<?php if (get_field('school_phone', 'option') ) { echo ' | '; the_field('school_phone', 'option'); } ?>
+						<?php if (get_field('school_email_address', 'option') ) { echo ' | <a href="mailto:' . get_field('school_email_address', 'option') . '">' .  get_field('school_email_address', 'option') . '</a>' ; } ?>
 						<span id="privacy-link"> | <a href="/privacy-policy/">Privacy Policy</a></span>
 					</p>
 
