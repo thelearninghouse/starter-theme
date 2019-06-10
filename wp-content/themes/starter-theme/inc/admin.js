379a3d6ac8682c028fba19476edf001dbca8c48a
Kurt Rank <krank@learninghouse.com>
2019-06-10 16:40:46 -0400

    fix(admin): Only show upload warning if image is too large

--- wp-content/themes/starter-theme/inc/admin.js
+++ wp-content/themes/starter-theme/inc/admin.js
@@ -1,18 +1,24 @@
 (function($) {
-  $.extend(wp.Uploader.prototype, {
-    success: function(file_attachment) {
-      console.log(file_attachment);
-      var alertText = 'WARNING: This uploaded image does not meet our performance standards! \n \n';
-      if (file_attachment.attributes.height > 1600) {
-        alertText += 'Height should not exceed 1600px \n \n';
-      }
-      if (file_attachment.attributes.width > 1600) {
-        alertText += 'Width should not exceed 1600px \n \n';
-      }
-      if (file_attachment.attributes.filesizeInBytes > 512000) {
-        alertText += "File size should not exceed 512kB \n \n";
-      }
-      alert(alertText);
-    }
-  });
+	$.extend(wp.Uploader.prototype, {
+		success: function(file_attachment) {
+			var error = false;
+			var alertText =
+				"WARNING: This uploaded image does not meet our performance standards! \n \n";
+			if (file_attachment.attributes.height > 1600) {
+				error = true;
+				alertText += "Height should not exceed 1600px \n \n";
+			}
+			if (file_attachment.attributes.width > 1600) {
+				error = true;
+				alertText += "Width should not exceed 1600px \n \n";
+			}
+			if (file_attachment.attributes.filesizeInBytes > 512000) {
+				error = true;
+				alertText += "File size should not exceed 512kB \n \n";
+			}
+			if (error) {
+				alert(alertText);
+			}
+		}
+	});
 })(jQuery);
