e6625214c75746b15957854059ec8a7ed0fa5ab6
Brian Maier Jr <brimaidesigns@gmail.com>
2019-08-29 16:06:29 -0400

    + tiny slider

--- wp-content/themes/starter-theme/theme.config.js
+++ wp-content/themes/starter-theme/theme.config.js
@@ -1,30 +1,30 @@
 module.exports = {
-  directoryName: "starter-theme",
-  selectors: {
-    accordion: ".accordion",
-    mixItUp: ".mixitup",
-    socialShare: ".social-link",
-    sticky: ".sticky",
-    slider: ".glide",
-    dialog: ".dialog",
-    smoothScroll: ".smooth-scroll"
-  },
-  useVue: true,
-  purgecssWhitelist: [
-    "requestinfo",
-    "menu-main-menu",
-    "current_page_item",
-    "fieldset",
-    "legend",
-    "label",
-    "current-menu-item",
-    "ol",
-    "columns-2"
-  ],
-  purgecssWhitelistPatterns: [
-    /^elp(_.*)?$/,
-    /^keep-nested-children/,
-    /^largeCopy/,
-    /^fancyNumbers/
-  ]
+	directoryName: "starter-theme",
+	selectors: {
+		accordion: ".accordion",
+		mixItUp: ".mixitup",
+		socialShare: ".social-link",
+		sticky: ".sticky",
+		slider: ".slider",
+		dialog: ".dialog",
+		smoothScroll: ".smooth-scroll"
+	},
+	useVue: true,
+	purgecssWhitelist: [
+		"requestinfo",
+		"menu-main-menu",
+		"current_page_item",
+		"fieldset",
+		"legend",
+		"label",
+		"current-menu-item",
+		"ol",
+		"columns-2"
+	],
+	purgecssWhitelistPatterns: [
+		/^elp(_.*)?$/,
+		/^keep-nested-children/,
+		/^largeCopy/,
+		/^fancyNumbers/
+	]
 };
